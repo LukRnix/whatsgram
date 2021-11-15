7e5e978db22b8544c5d1a447b5e198987f88852a
marvinl00 <marvin.leine@icloud.com>
2021-11-15 14:32:01 +0100

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.whatsgram.app',
+  appName: 'vite-template-solid',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
